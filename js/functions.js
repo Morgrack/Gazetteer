4daa641a2c7d51168193b4fa72cec46479fc502e
Morgrack <morgan.pukis@hotmail.com>
2026-03-18 16:20:39 +0000

    made forecast modal slightly more responsive

--- js/functions.js
+++ js/functions.js
@@ -397,6 +397,7 @@ function drawForecastGraph(forecasts)
             options: 
             {
                 responsive: true,
+                maintainAspectRatio: false,
                 plugins: 
                 {
                     legend: 
